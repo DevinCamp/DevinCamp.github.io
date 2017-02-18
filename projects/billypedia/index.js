817b1a04f54cb0fee1c6730a9b080ee1e5e3551d
Devin Campbell <campbelldevin1@hotmail.com>
2017-02-18 03:06:48 +0000

    Changed

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         
         // EXAMPLE: Looping over top rated recordings; replace with your code //
                 
-        
+       
             
             
          let recordings = data.discography.recordings;
@@ -29,6 +29,7 @@ $(document).ready(function() {
          $topRatedImage.appendTo('#sidebar');
         
          let $top = _.map(topRated, function(recording) {
+             let $art = recording.art;
              return $section.append($('<li>').attr('class', 'top-recording')
                  .append($('<div>').text("Title: " + recording.title))
                  .append($('<div>').text("Artist: " + recording.artist))
@@ -37,7 +38,16 @@ $(document).ready(function() {
                  
                  
         });
-        
+                                                        // clicks on list item //
+        //  _.each($top,function(x){
+        //         ('.top-recording').on('click',function(event){
+                   
+        //           //////clicks, cause photo to self-identify index in top rated charts
+                   
+        //             $('#top-rated-image').attr('src', topRated[_.indexOf($top,x)].art);
+        //         }).appendTo(x);
+        //     })
+          
        
         
         
@@ -55,15 +65,25 @@ $(document).ready(function() {
          $sectionlist.appendTo($('#section-recordings'));
 
                                                              //RECORDINGS
-         _.map(recordings, function(recording) {
-             return $sectionlist.append($('<li>').attr('class', 'recording')
+       let $rec = _.map(recordings, function(recording) {
+           
+            return $sectionlist.append($('<li>').attr('class', 'recording')
                  .append($('<div>').text("Title: " + recording.title))
                  .append($('<div>').text("Artist: " + recording.artist))
                  .append($('<div>').text("Release: " + recording.release))
                  .append($('<div>').text("Year: " + recording.year)));
                  
          });
-         
+           
+        //          $('.recording').on('click', function(event) {
+        //      $('#recording-image').fadeOut(400, function(event) {
+                 
+                    
+        //               $('#recording-image').attr('src', );
+        //          })
+        //          .fadeIn(500);
+        //  });
+        
         
                                                         // Image change  //
          let count = 0;
@@ -78,18 +98,16 @@ $(document).ready(function() {
                  .fadeIn(500);
          });
         
-          let $img = _.map(topRated,function(recording) {
+        
+        
+        
+        
+          let $img = _.map(recordings,function(recording) {
             return recording.art;
         });
         console.log($img)
         
-         $('.top-recording').on('click', function() {
-             $('#top-rated-image').fadeOut(400, function() {
-                     if($img === '#top-recording'.length )
-                     return $('#top-rated-image').attr('src', $img);
-                 })
-                 .fadeIn(500);
-         });
+        
           
           
           
@@ -133,117 +151,7 @@ $(document).ready(function() {
  
  
  
-//   $('<section>').attr('id','section-rider').append($('<h3>')).text('Billy\'s Rider').appendTo($('#sections'));
-        
-        
-        
-        
-//         let topRated = data.discography.topRated;
-//             var $topRecord =_.map(topRated, function(recording) {
-//                 return _.map(recording,function(value,attribute){
-//                     if(attribute!='art'){
-//                     var string = [attribute]+' : '+ [value] ;
-//                     return ($('<div>').attr('class', attribute).text(string));
-//                     }        
-//                 });
-//             });
-//             _.each($topRecord,function(x){
-//                 ($('<ul>')).attr('class','recording').appendTo($('#list-top-rated')).on('click',function(event){
-                   
-//                   //////clicks, cause photo to self-identify index in top rated charts
-                   
-//                     $('#top-rated-image').attr('src', topRated[_.indexOf($topRecord,x)].art);
-//                 }).append(x);
-//             })
-        
-//         // // ///////Top albums list
-//         const $recordings = ($('<section>')).attr('id','section-recordings').text('Recordings').appendTo('#sidebar');
-//         $('<ul>').attr('id','list-recordings').appendTo('#section-recordings');
-//         let recordings = data.discography.recordings;
-//             var $recordList =_.map(recordings, function(recording) {
-//                 return _.map(recording,function(value,attribute){
-//                     if(attribute!='art'){
-//                     var string = [attribute]+' : '+ [value] ;
-//                     return ($('<div>').attr('class', attribute).text(string));
-//                     }        
-//                 });
-//             });
-//             _.each($recordList,function(record){
-//                 ($('<li>')).attr('class','recording').appendTo($('#list-recordings')).on('click',function(event){
-                    
-//                     //////clicks, cause photo to self-identify index in records charts
-//                     $('#recording-image').attr('src',recordings[_.indexOf($recordList,record)].art)
-//                 }).append(record);
-//             })
 
-        
-        
-//         /////////////////
-        
-        
-//         // // pictures to recording lists
-//         $('<div>').attr('id','image-container-top-rated').attr('class','image-container').appendTo('#header-top-rated');
-//              $('<img>').attr('id','top-rated-image').attr('src',topRated[0].art).attr('class','image').appendTo('#image-container-top-rated');                                                           
-        
-        
-//         $('<div>').attr('id','image-container-recording').attr('class','image-container').prependTo('#list-recordings');
-//              $('<img>').attr('id','recording-image').attr('src',recordings[0].art).attr('class','image').appendTo('#image-container-recording');                                                           
-        
-//         /**
-//                     This picture switcher functions are in the above sections, where I populate the page with albums
-        
-        
-//         */
-        
-        
-        
-//         // ///////pic cycler
-//         // let billyPics = data.images.billy;
-//         // var  picCounter = 0;
-//         // $('#image-billy').on('click', function(event){
-            
-//         //     picCounter<billyPics.length-1 ? picCounter++ : picCounter=0;
-//         //     $('#image-billy').attr('src',billyPics[picCounter]);
-//         // });
-        
-//         // const pacifier = opspark.makePacifier($('#image-container-billy')[0]);
-        
-        
-// var createTable = function(itemList){
-//         var createRow = function(item){
-//         var $row = $("<tr>");
-//         var $drum = $("<td>").text(item.type);
-//         var $dimension = $("<td>").text(item.desc);
-//         $row.append($drum);
-//         $row.append($dimension);
-//         return $row;
-//     }
-//     var $table = $("<table>");
-//     var $rows = itemList.map(createRow);
-//     $table.append($rows);
-//     return $table;
-// };
-
-// createTable(data.rider).appendTo("#section-rider");
-        
-        
-        
-        
-        
-        
-//         $('td').css('border','2px').css('border-style','solid').css('color','gold');
-//         $('ul').css('width','200px');
-//         $('li').css('display','block');
-//         $('.recording').css('border-top','2px').css('border-style','solid')
-//         $('.recording,#list-top-rated,#section-recordings,#list-recordings').css('padding-left','0px');
-//         $('main').css('padding-top','10px');
-//         $('.content').css('margin-right','0px')
-//         $('main,body').css('background-color', 'plum').css;
-//         $('#sections,nav,div,#sidebar').css('background-color', 'blue').css('border-radius', '4px');
-//          $('p').css('color', 'gold').css('padding-left', '10px');
-//          $('h2,h3,h4,header,a,li,ul,#section-recordings').css('color', 'gold').css('font-style', 'italic');
-//         $('div').css('color', 'gold').css('font-style','italic').css('padding-bottom','2px');
- 
  
  
         // YOUR CODE ABOVE HERE //
--- projects/product-project/js/products.js
+++ projects/product-project/js/products.js
@@ -4,32 +4,137 @@ $(document).ready(function () {
   // ALL YOUR CODE GOES BELOW HERE //
     $.getJSON('data/product.json', function (data) {
  console.log(data)
-      
-  
-      
-       let $section = $('<section>').attr('id', 'top');
-      $section.appendTo($('main'));
  
-         let list = data;
-          $section.append($('<article>')).attr('class', 'row');
-          
-        _.map(list, function(item){
-                let $x = $('<section>').attr('class', 'col-md-9').appendTo('article');
-                $('<img>').attr( 'src','img/product/thumbs/'+item.image).attr('class', 'col-md-3').appendTo($x);
-                $('<div>').text("Description:  " + item.desc).appendTo($x);
-                $('<div>').text("Price:  " + item.price).appendTo($x);
-                $('<div>').text("Color:  " + item.color).appendTo($x);            
-                $('<div>').text("Stock : " + item.stock).appendTo($x); 
  
-            });
-            
-            
-           ('col-md-9').on('click'(function( ){
-             
-           })) 
+     $('#search').css('margin-top','20px').css('margin-bottom','5px');
+   
+      // search bar function
+      
+function search(data, target){
+    function find(data, target){
+        if(typeof data ==='string'){
+            return data.toUpperCase().includes(target.toUpperCase());
+        }else if(typeof data==='object'){
+            return _.some(data,function(val,ind,coll){
+                return find(val,target);
+            })
+        }
+    }
+    return _.filter(data,function(data){
+        return find(data,target);
+    })
+              
+}
+$('#search').on('click', function() {
+    let ser = search(data, $('input:text').val());
+    display(ser);
+});
+
+
+function display(list) {
+    $('main').empty();
+    if (list.length === 0) {
+        $('<div>').text('No Items Found').attr('class', 'text-center').css('font-size', '20px').css('font-weight', 'bold').appendTo('main');
+    }
+    _.map(list, function(list) {
+        let row = $('<article>').attr('class', 'row');
+        let thumbnail = 'img/product/thumbs/' + list.image;
+        $('<img>').attr('src', thumbnail).attr('class', 'col-md-2').appendTo(row);
+        let mainDesc = $('<section>').attr('class', 'col-md-6').appendTo(row);
+        $('<desc>').text(list.desc).css('color', 'white').appendTo(mainDesc);
+
+        $('main').append(popup(list));
+        row.on('click', function() {
+            $("#" + list.id + "-modal").modal();
+        });
+
+        row.appendTo('main');
+        $('<div>').text('Price: $' + list.price).css('color', 'white').appendTo(mainDesc).css('font-size', '12px');
+        if (list.stock < 10) {
+            $('<section>').text("Only " + list.stock + " left in-stock!!! BUY IT NOW").css('color', 'red').css('font-size', '20px').appendTo(mainDesc);
+        }
+        else {
+            $('<section>').text(list.stock + " left in-stock").css('color', 'white').appendTo(mainDesc);
+        }
+        //  $('<button>').text("Buy").appendTo(row).on('click',function(event){alert('You bought this item'+product.desc)}).attr('class','text-center');
+    });
+
+}
+display(data);
+
+
+
+
+$('#filter').css('margin-top', '20px').css('margin-bottom', '4px');
+var fTypes = _.unique(_.map(data, function(val, ind, col) {
+    return val.type;
+}));
+_.map(fTypes, function(val) {
+    $('<option>').text(val).appendTo('#filter');
+});
+
+
+
+function filter(data, target) {
+    return _.filter(data, (el) => {
+
+        return el.type === target;
+    });
+}
+
+
+$('select').change(function() {
+    if ($('select option:selected').text() === 'All') {
+        display(data);
+        return;
+    }
+    display(filter(data, ($('select option:selected').text())));
+});
+
+
+
+
+
+
+
+
+function popup(product) {
+    let first = $('<div>').attr('class', 'modal fade').attr('role', "dialog").attr('id', product.id + "-modal");
+
+    let second = $('<div>').attr('class', 'modal-dialog');
+    first.append(second);
+
+    let third = $('<div>').attr('class', 'modal-content');
+    second.append(third);
+
+    let forth = $('<div>').attr('class', 'modal-header');
+    third.append(forth);
+    forth.append($('<button>').attr('class', 'close').attr('data-dismiss', 'modal').wrapInner('&times;'));
+    forth.append($('<h4>').attr('class', 'modal-title').text(product.desc));
+
+    let mBody = $('<div>').attr('class', 'modal-body  ');
+    mBody.appendTo(forth);
+    mBody.append($('<img>').attr('src', "img/product/" + product.image).attr('class', 'col-md-3'));
+
+    let desc = $('<div>').attr('class', 'col-md-9');
+    desc.wrapInner("<p><b>Available Colors:</b> " +
+        product.availableColors + "</p><p><b>Specifications: </b>" +
+        product.specs + "</p><p><b>Price:</b> " +
+        '$' + product.price + "</p>");
+    desc.append($('<p>').text(product.stock + ' left in stock'));
+
+    mBody.append(desc);
+    return first;
+}
+
+        
          
               
-        
+        // $('li').on('click', (e) => {
+        //  let prodId = $(e.currentTarget).attr('data-product-id')
+        //  let product = findProductById(productId);
+        //  showProductDetails(product);
+        // });
    
    
    
